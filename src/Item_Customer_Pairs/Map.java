1ca7d243f28552d1b8778b403ae5773ab1177efa
Muhannad Mughrabi <mughrabi@Muhannads-MacBook-Pro.local>
2022-10-20 01:44:32 -0500

    add without in mapper

--- src/Item_Customer_Pairs/Map.java
+++ src/Item_Customer_Pairs/Map.java
@@ -1,8 +1,6 @@
 package Item_Customer_Pairs;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map.Entry;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -10,47 +8,19 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class Map extends Mapper<LongWritable, Text, Pair, IntWritable> {
-	  
-	HashMap<Pair, Integer> hashmap = new HashMap<>();
 
-	@Override
-	protected void setup(
-			Mapper<LongWritable, Text, Pair, IntWritable>.Context context) {
-		hashmap = new HashMap<>();
-	}
+    @Override
+    protected void map(LongWritable key, Text value,
+                       Mapper<LongWritable, Text, Pair, IntWritable>.Context context) throws IOException, InterruptedException {
+        String line = value.toString().trim();
+        String[] input = line.split(" ");
 
-	@Override
-	protected void map(LongWritable key, Text value,
-			Mapper<LongWritable, Text, Pair, IntWritable>.Context context) {
-		String line = value.toString().trim();
-		String[] input = line.split(" ");
+        for (int i = 0; i < input.length; i++) {
+            for (int j = i + 1; j < input.length && !input[i].equals(input[j]); j++) {
+                context.write(new Pair(input[i], input[j]), new IntWritable(1));
+                context.write(new Pair(input[i], "*"), new IntWritable(1));
+            }
+        }
+    }
 
-		for (int i = 0; i < input.length; i++) {
-			for (int j = i + 1; j < input.length && !input[i].equals(input[j]); j++) {
-				Pair p = new Pair(input[i], input[j]);
-				if (hashmap.get(p) == null) {
-					hashmap.put(p, 1);
-				} else {
-					hashmap.put(p, hashmap.get(p) + 1);
-				}
-				
-				p = new Pair(input[i], "*");
-				if (hashmap.get(p) == null) {
-					hashmap.put(p, 1);
-				} else {
-					hashmap.put(p, hashmap.get(p) + 1);
-				}
-			}
-		}
-	}
-
-	@Override
-	protected void cleanup(
-			Mapper<LongWritable, Text, Pair, IntWritable>.Context context)
-			throws IOException, InterruptedException {
-		super.cleanup(context);
-		for (Entry<Pair, Integer> entry : hashmap.entrySet()) {
-			context.write(entry.getKey(), new IntWritable(entry.getValue()));
-		}
-	}
 }
